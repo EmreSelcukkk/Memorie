62e434ba38a0ea824b70c432f3b8e957ead4ac97
EmreSelcukkk <163849111+EmreSelcukkk@users.noreply.github.com>
2025-02-10 22:29:20 +0300

    ikinci commit

--- app/src/main/java/com/example/memories/MainActivity.kt
+++ app/src/main/java/com/example/memories/MainActivity.kt
@@ -17,24 +17,5 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-
-        var listem = arrayListOf<String>("emre","ali","murat","adem","karabulut","özgül")
-        var bosListe = listOf<String>()
-        var i = 0
-        var j =0
-        var x =0
-
-        for( i in listem.size){
-            bosListe=listem[i].lowercase()
-            i++
-        }
-
-       for (x in bosListe.size){
-            if(bosListe.contains("a")){
-            j++ }
-                   x++
-        }
-        for (i in bosListe.size){
-        println(bosListe[i])}
     }
 }
