179d52ddf3938c663c088fabddb0ef7acd69cf33
EmreSelcukkk <163849111+EmreSelcukkk@users.noreply.github.com>
2025-02-20 09:57:04 +0300

    commit 2

--- app/src/main/java/com/example/memories/HomePage.kt
+++ app/src/main/java/com/example/memories/HomePage.kt
@@ -5,12 +5,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.memories.databinding.ActivityHomePageBinding
 
 class HomePage : AppCompatActivity() {
+
+
+    private lateinit var binding : ActivityHomePageBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityHomePageBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_home_page)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
